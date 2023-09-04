513095553727e4e32bf33ab833ad988194b07651
Malay-24 <malaybajpai24@gmailcom>
2023-09-04 12:41:35 +0530

    viro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React from "react";
 function App() {
+  const[btc,setBtc]=React.useState(0)
+  const[doge,setDoge]=React.useState(0)
+  const[ripple,setRipple]=React.useState(0)
+  const[bit,setBit]=React.useState("")
+  const[dog,setDog]=React.useState("")
+  const[rip,setRip]=React.useState("")
+
+  const handleRip=(e)=>{
+    setRip(e.target.value)
+  }
+
+  const handleBit=(e)=>{
+    setBit(e.target.value)
+  }
+  const handleDog=(e)=>{
+    setDog(e.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>BTC</p>
+     <p>$25000</p>
+     <input value={bit} type='number' placeholder='Qty' onChange={handleBit}/>
+     <button onClick={()=>setBtc(btc+1)}>ADD</button>
+
+     <p>Doge</p>
+     <p>$0.75</p>
+     <input value={dog} type='number' placeholder='Qty' onChange={handleDog}/>
+     <button onClick={()=>setDoge(doge+1)}>ADD</button>
+
+     <p>Ripple</p>
+     <p>$1.5</p>
+     <input value={rip} type='number' placeholder='Qty' onChange={handleRip}/>
+     <button onClick={()=>setRipple(ripple+1)}>ADD</button>
+
+     <div>
+      <p>MyCart</p>
+      <p>Qty</p>
+      <p>{bit}</p>
+      <p>Amount</p>
+      <p>{25000*btc}</p>
+      </div>
     </div>
+
+   
   );
 }
 
